8c3d9030954f893183a84d7d6da9b0bca29026b0
Zbigniew Braniecki <gandalf@mozilla.com>
2012-05-18 15:32:08 +0200

    make the entity diff work on nodes and values alike

--- l10ndiff/entity.py
+++ l10ndiff/entity.py
@@ -1,74 +1,100 @@
+import sys
 
-class EntityDiff(dict):
-    """
-    EntityDiff is a dictionary that
-    represents a difference between two Entity objects.
-    It is made of hunks that applied onto one entity should
-    result in a second entity, and applied onto a second
-    entity with reverse option should give the first.
-    """
+if sys.version >= '3':
+    basestring = str
+    string = str
+else:
+    string = unicode
 
-    def __init__(self, id):
+def intersect(*args):
+    r = set(args[0])
+    for arg in args[1:]:
+        r &= set(arg)
+    return list(r)
+
+def equalseq(iterator):
+    try:
+        iterator = iter(iterator)
+        first = next(iterator)
+        return all(first == rest for rest in iterator)
+    except StopIteration:
+        return True
+
+def equaltypeseq(iterator):
+    try:
+        iterator = iter(iterator)
+        first = next(iterator)
+        return all(type(first) == type(rest) for rest in iterator)
+    except StopIteration:
+        return True
+
+class NodeDiff(dict):
+    pass
+
+class EntityDiff(NodeDiff):
+    def __init__(self, id=None):
         self.id = id
-        self.params = {}
 
-    def empty(self):
-        return not len(self)
-
-    def value(self):
-        return self['value']
-
-"""
- a list of parameters of an entity to be compared.
-
- value[0] is a getter, value[1] is a setter - if not
- defined, key is used.
-"""
-test_params = {
-   'id' : (),
-   'value': ('values', 'value')
-}
-
-def entity_diff(self, entities):
-    ediff = EntityDiff(self.id)
-    if is_entity(entities):
-        entities = (self, entities)
-    else:
-        try:
-            entities = [self,] + entities
-        except TypeError:
-            entities = (self,) + entities
-    for key, test in test_params.items():
-        last = None
-        value = []
-        is_value_different = False
-        for entity in entities:
-            getter = test[0] if len(test)>0 else key
-            param = getattr(entity, getter)
-            if hasattr(param, '__call__'):
-                nval = param.__call__()
+def diff_values(*args):
+    if isinstance(args[0], basestring):
+        return args
+    if isinstance(args[0], list):
+        fdiff = []
+        l = max([len(i) for i in args])
+        for i in range(0, l):
+            vals = [j[i] for j in args]
+            if not equalseq(vals):
+                fdiff.append(vals)
             else:
-                nval = param
-            if not is_value_different and last:
-                is_value_different = last!=nval
-            value.append(nval)
-            last = nval
-    
-        if is_value_different:
-            ediff[key] = tuple(value) 
+                fdiff.append(None)
+        return fdiff
+    if isinstance(args[0], dict):
+        keys = intersect(*[i.keys() for i in args])
+        fdiff = {}
+        for key in keys:
+            fdiff[key] = diff_values(*[i[key] for i in args])
+        return fdiff
+    if hasattr(args[0], '_fields'):
+        return diff_nodes(*args)
+    return args
+
+def diff_nodes(*args):
+    fields = []
+    for node in args:
+        fields.append(node._fields)
+    fields = intersect(*fields)
+    ndiff = NodeDiff()
+    for field in fields:
+        fdiff = []
+        for node in args:
+            if not hasattr(node, field):
+                fdiff.append(None)
+            else:
+                fdiff.append(getattr(node, field))
+        if equalseq(fdiff):
+            pass
+        else:
+            print('---')
+            print('nequal')
+            print(field)
+            print(hasattr(fdiff[0], '_fields'))
+            print(type(fdiff[0]))
+            print('====')
+            if len(set(map(type, fdiff))) == 1:
+                print('equal types')
+                if hasattr(fdiff[0], '_fields'):
+                    ndiff[field] = diff_nodes(*fdiff)
+                else:
+                    ndiff[field] = diff_values(*fdiff)
+            else:
+                print('different types')
+                print(fdiff)
+                ndiff[field] = fdiff
+    return ndiff
+
+def entities(*args):
+    if len(args) < 2:
+        raise TypeError('diff() must have at least two arguments.')
+    ediff = diff_nodes(*args)
     return ediff
-
-
-def entity_apply_diff(self, ediff, reverse=False,
-                      source=0, result=1, ignore_mismatch=False):
-    if reverse:
-        source, result = (result, source)
-    for key, hunk in ediff.items():
-        test = test_params[key]
-        getter = test[0] if len(test)>0 else key
-        curval = getattr(self, getter)
-        if ignore_mismatch is False and curval!=hunk[source]:
-            raise Exception('Hunk "%s" mismatch' % key)
-        setter = test[1] if len(test)>1 else key
-        setattr(self, setter, hunk[result])
 
