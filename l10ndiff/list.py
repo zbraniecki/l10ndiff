9c3acda1cefa46f6510dda5a1918e178b02f9694
Zbigniew Braniecki <gandalf@mozilla.com>
2012-05-29 17:46:44 +0200

    add odict package and list diffing

---
+++ l10ndiff/list.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+from .entity import entities
+
+def intersect(*args):
+    r = set(args[0])
+    for arg in args[1:]:
+        r &= set(arg)
+    return list(r)
+
+def diff_lists(*lists, **kval):
+    values = kval.get('values', True)
+    keys = intersect(*[l.keys() for l in lists])
+    ldiff = EntityListDiff()
+    for key in keys:
+        kdiff = {'elem': entities(*[l[key] for l in lists], values=values), 'flags': set(('present',))}
+        ldiff[key] = kdiff
+    for i,l in enumerate(lists):
+        for k in l.keys():
+            if k not in keys:
+                kdiff = ldiff.get(k, [])
+                if not kdiff:
+                    kdiff = {'elem': [None]*len(lists), 'flags': set()}
+                kdiff['elem'][i] = l[k]
+                kdiff['flags'].add('added')
+                ldiff[k] = kdiff
+    return ldiff
+
+class EntityListDiff(OrderedDict):
+    def __init__(self, id=None):
+        self.id = id
+        super(EntityListDiff, self).__init__(self)
+
+    # pos - may be a number or a tuple ('after','id') or ('before','id')
+    def add(self, flag, ediff, id, pos=None):
+        self[id] = {'elem': ediff, 'flags': [flag], 'pos': pos}
+
+
+def lists(*lists, **kwalues):
+    values = kwalues.get('values', True)
+    eldiff = diff_lists(*lists, values=values)
+    return eldiff
+    
